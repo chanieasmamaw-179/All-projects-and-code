26258a47fd51991324526f8cb8d9c39ecbefb64e
Asmamaw Yehun <chanieasmamaw>
2024-09-17 12:01:53 +0200

    Initial commit

---
+++ SE105.2 Object Oriented Programming  /Vehicle.py
@@ -0,0 +1,12 @@
+class Vehicle:
+    def __init__(self, registration):
+        self.registration = registration
+
+    def get_registration(self, odometr, fule):
+        return self.registration
+
+
+odometr_get() = Vihcle()
+fule_get() = Vihcle()
+print_get_registration()
+print_get_registration()
